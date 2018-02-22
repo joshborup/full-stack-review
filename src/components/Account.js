4966b34df9db934f9b804261e2d543a57bb213da
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-02-22 11:17:53 -0700

    added accountd

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import './account.css'
 
 export default class Account extends Component {
     render() {
+        const { picture, name , email, amount, id} = this.props;
         return (
-            <h1>Account</h1>
+            <div className='account'>
+                <h1>Account</h1>
+                <img src={picture}/>
+                <div><span className='bold'>Name</span></div>
+                <div><span className='bold'>Email</span></div>
+                <div><span className='bold'>Amount</span></div>
+                <div><span className='bold'>ID</span></div>
+            </div>
         );
     }
 }
